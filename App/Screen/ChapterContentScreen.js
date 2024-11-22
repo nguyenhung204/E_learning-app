2e4b54e5272d40d9365c698ea9e1861de260dda8
ChienHandsome <cerkvena291@gmail.com>
2024-11-22 12:48:51 +0700

    [Fixbug] error when load content

--- App/Screen/ChapterContentScreen.js
+++ App/Screen/ChapterContentScreen.js
@@ -9,7 +9,57 @@ import { useUser } from '@clerk/clerk-expo'
 import { UserPointsContext } from '../Context/UserPointsContext'
 
 export default function ChapterContentScreen() {
+  const param = useRoute().params;
+  const { setIsChapterComplete, setCompletedChapters } = useContext(CompleteChapterContext);
+  const {user} = useUser();
+  const {userPoints, setUserPoints} = useContext(UserPointsContext);
+
+  useEffect(() => {
+    console.log("ChapterID", param.chapterId);
+    console.log('RecordId',param.userCourseRecordId )
+    }, [param])
+
+    const onChapterCompleted = () => {
+      const contentLength = param.content?.length;
+      const points = Number(userPoints);
+      const totalPoints = points + contentLength * 10;
+      
+      if (Number.isNaN(totalPoints)) {
+        console.error('Total points calculation resulted in NaN');
+        return;
+      }
   
+      // Optimistic update
+      setIsChapterComplete(true);
+      setCompletedChapters(prev => [...prev, param.chapterId]);
+      setUserPoints(totalPoints);
+  
+      // API call
+      MarkChapterCompleted(
+        param.chapterId,
+        param.userCourseRecordId, 
+        user.primaryEmailAddress.emailAddress, 
+        totalPoints
+      ).then(resp => {
+        if(resp) {
+          Toast.show({
+            type: 'success',
+            text1: 'Congratulation !!!',
+            text2: 'You completed this chapter !!! '
+          });
+        }
+      }).catch(error => {
+        // Rollback on error
+        setIsChapterComplete(false);
+        setCompletedChapters(prev => prev.filter(id => id !== param.chapterId));
+        setUserPoints(points);
+        Toast.show({
+          type: 'error',
+          text1: 'Failed to mark chapter as completed',
+          text2: 'Please try again'
+        });
+      });
+    };
 
 
   return (
